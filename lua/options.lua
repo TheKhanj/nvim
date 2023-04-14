542cf38dd34f65e38de826c6d530f828e75c4719
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-04-14 03:40:26 +0330

    chore: disable expandtab option

--- lua/options.lua
+++ lua/options.lua
@@ -27,7 +27,6 @@ vim.opt.cindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.shiftround = true
